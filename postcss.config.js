d010a498fadbf69c2042ba0a4d80351ae497e7c5
jbcanillo <jbcanillo@gmail.com>
2026-03-26 07:31:48 +0000

    fix: always include cssnano with calc disabled to resolve DaisyUI build errors

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' && {
-      cssnano: {
-        preset: ['default', {
-          calc: false,
-        }],
-      },
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    cssnano({
+      preset: ['default', { calc: false }],
     }),
-  },
+  ],
 };
